390bcd8b69b728b970bbb3c569840f446530d1e9
sandorbence <szaniszlo47@gmail.com>
2024-06-22 10:22:20 +0200

    Add moving background

--- main.py
+++ main.py
@@ -9,11 +9,14 @@ screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 running = True
 dt = 0
+i = 0
+background_move_speed = 3
+player_move_speed = 300
 
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
 bg_image = pygame.image.load('Resources/Images/background.png')
-bg_image = pygame.transform.scale(bg_image,(width,height))
+bg_image = pygame.transform.scale(bg_image, (width, height))
 
 while running:
     # poll for events
@@ -22,24 +25,25 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # fill the screen with a color to wipe away anything from last frame
-    #screen.fill("purple")
-    screen.blit(bg_image,(0,0))
-
+    screen.fill((0, 0, 0))
+    screen.blit(bg_image, (i, 0))
+    screen.blit(bg_image, (width+i, 0))
+    if i <= -width:
+        i = 0
+    i -= background_move_speed
     pygame.draw.circle(screen, "red", player_pos, 40)
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
+        player_pos.y -= player_move_speed * dt
     if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
+        player_pos.y += player_move_speed * dt
     if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
+        player_pos.x -= player_move_speed * dt
     if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
+        player_pos.x += player_move_speed * dt
 
-    # flip() the display to put your work on screen
-    pygame.display.flip()
+    pygame.display.update()
 
     # limits FPS to 60
     # dt is delta time in seconds since last frame, used for framerate-
