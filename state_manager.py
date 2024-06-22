89c0afc5ac17afe1462cf06f7e435763c207c06c
sandorbence <szaniszlo47@gmail.com>
2024-06-22 18:49:17 +0200

    Add main menu

--- state_manager.py
+++ state_manager.py
@@ -1,17 +1,18 @@
-from game_state import GameState
+from main_menu import MainMenu
+
 
 class StateManager:
     def __init__(self):
         self.states = {
-            
+            'main_menu': MainMenu
         }
         self.current_state = self.states['main_menu']
-        
+
     def change_state(self, new_state):
         self.current_state = self.states[new_state]
-        
+
     def update(self):
         self.current_state.update()
-        
+
     def render(self):
         self.current_state.render()
