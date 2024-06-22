e9d1c8e536c84d2c2107368b259385c866c1865c
sandorbence <szaniszlo47@gmail.com>
2024-06-22 12:05:57 +0200

    Add screen boundaries

--- player.py
+++ player.py
@@ -2,6 +2,7 @@ import pygame
 
 from constants import *
 
+
 class Player(pygame.sprite.Sprite):
     def __init__(self, image, position):
         super().__init__()
@@ -11,11 +12,11 @@ class Player(pygame.sprite.Sprite):
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_w]:
+        if keys[pygame.K_w] and self.rect.top > 0:
             self.rect.y -= PLAYER_MOVE_SPEED * dt
-        if keys[pygame.K_s]:
+        if keys[pygame.K_s] and self.rect.bottom < SCREEN_HEIGHT:
             self.rect.y += PLAYER_MOVE_SPEED * dt
-        if keys[pygame.K_a]:
+        if keys[pygame.K_a] and self.rect.left > 0:
             self.rect.x -= PLAYER_MOVE_SPEED * dt
-        if keys[pygame.K_d]:
+        if keys[pygame.K_d] and self.rect.right < SCREEN_WIDTH:
             self.rect.x += PLAYER_MOVE_SPEED * dt
