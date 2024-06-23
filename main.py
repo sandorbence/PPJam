69a649b768902cabd6627b5efe15c10cf7513cd8
sandorbence <szaniszlo47@gmail.com>
2024-06-23 20:29:00 +0200

    Add background music

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from constants import *
 pygame.init()
 pygame.mixer.init()
 
+pygame.mixer.music.load('Resources/Sounds/background_music.mp3')
+pygame.mixer.music.set_volume(0.5)
+pygame.mixer.music.play(-1)
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
